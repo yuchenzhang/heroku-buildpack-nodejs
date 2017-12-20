25c08786dbeecd52bdc8de2379073df6201ea4e8
Yuchen Zhang <yc@aihehuo.com>
2017-12-20 21:30:43 +0800

    added predownload option

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -36,16 +36,16 @@ install_nodejs() {
   fi
 
   echo "Downloading and installing node $number from $url..."
-  local code=$(curl "$url" -L --silent --fail --retry 5 --retry-max-time 15 -o /tmp/node.tar.gz --write-out "%{http_code}")
-  if [ "$code" != "200" ]; then
-    echo "Unable to download node: $code"
+  # local code=$(curl "$url" -L --silent --fail --retry 5 --retry-max-time 15 -o /tmp/node.tar.gz --write-out "%{http_code}")
+  # if [ "$code" != "200" ]; then
+    # echo "Unable to download node: $code"
     echo "Copying predownloaded /tmp/node-$version-$platform.tar.gz"
     if [ -e /tmp/node-$version-$platform.tar.gz ]; then
       cp /tmp/node-$version-$platform.tar.gz /tmp/node.tar.gz
     else
       false
     fi
-  fi
+  # fi
   tar xzf /tmp/node.tar.gz -C /tmp
   rm -rf $dir/*
   mv /tmp/node-v$number-$os-$cpu/* $dir
