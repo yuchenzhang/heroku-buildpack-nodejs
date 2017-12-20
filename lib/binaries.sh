77b254f2e8ee28ddc61b924f9786d6c553e13c26
Yuchen Zhang <yc@aihehuo.com>
2017-12-20 22:23:21 +0800

    fix

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -39,9 +39,6 @@ install_nodejs() {
   # local code=$(curl "$url" -L --silent --fail --retry 5 --retry-max-time 15 -o /tmp/node.tar.gz --write-out "%{http_code}")
   # if [ "$code" != "200" ]; then
     # echo "Unable to download node: $code"
-  if [ ! -e /tmp/node.tar.gz ]; then
-    false
-  fi
   tar xzf /tmp/node.tar.gz -C /tmp
   rm -rf $dir/*
   mv /tmp/node-v$number-$os-$cpu/* $dir
