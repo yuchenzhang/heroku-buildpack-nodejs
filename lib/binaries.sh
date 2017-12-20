164154bdeb4ffbf03f3f2929f1a35f7f8fb123fb
Yuchen Zhang <yc@aihehuo.com>
2017-12-20 22:27:25 +0800

    fix

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -36,14 +36,14 @@ install_nodejs() {
   fi
 
   echo "Downloading and installing node $number from $url..."
-  # local code=$(curl "$url" -L --silent --fail --retry 5 --retry-max-time 15 -o /tmp/node.tar.gz --write-out "%{http_code}")
-  # if [ "$code" != "200" ]; then
-    # echo "Unable to download node: $code"
+  local code=$(curl "$url" -L --silent --fail --retry 5 --retry-max-time 15 -o /tmp/node.tar.gz --write-out "%{http_code}")
+  if [ "$code" != "200" ]; then
+    echo "Unable to download node: $code"
+  fi
   tar xzf /tmp/node.tar.gz -C /tmp
   rm -rf $dir/*
   mv /tmp/node-v$number-$os-$cpu/* $dir
   chmod +x $dir/bin/*
-  # fi
 }
 
 install_iojs() {
