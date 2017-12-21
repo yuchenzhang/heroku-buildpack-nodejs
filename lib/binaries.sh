43f262cba2ab7f387825a0492b57baa0282775a6
Yuchen Zhang <yc@aihehuo.com>
2017-12-21 15:49:22 +0800

    updated

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -38,14 +38,17 @@ install_nodejs() {
   # echo "Downloading and installing node $number from $url..."
   # local code=$(curl "$url" -L --silent --fail --retry 5 --retry-max-time 15 -o /tmp/node.tar.gz --write-out "%{http_code}")
   # if [ "$code" != "200" ]; then
-  #   echo "Unable to download node: $code"
+  #   echo "Unable to download node: $code" && false
   # fi
-  # tar xzf /tmp/node.tar.gz -C /tmp
-  if [ -d /tmp/node-v$number-$os-$cpu/ ]; then 
-    echo "find pre-downloaded folder"
-    rm -rf $dir/*
-    mv /tmp/node-v$number-$os-$cpu/* $dir
-    chmod +x $dir/bin/*
+  if [ -e /tmp/node-v$number-$os-$cpu.tar.gz ]; then 
+    echo "find pre-downloaded tar file"
+    tar xzf /tmp/node-v$number-$os-$cpu.tar.gz -C /tmp
+    if [ $dir != '']; then
+      echo "removing content in $dir"
+      rm -rf $dir/*
+      mv /tmp/node-v$number-$os-$cpu/* $dir
+      chmod +x $dir/bin/*
+    fi
   else
     false
   fi
